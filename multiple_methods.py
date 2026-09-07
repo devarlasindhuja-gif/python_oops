f9fd2342ed15155b876010b3051253b690ae961c
SINDHUJA DEVARLA <devarlasindhuja@gmail.com>
2026-09-07 15:54:05 +0530

    Create multiple_methods.py

---
+++ multiple_methods.py
@@ -0,0 +1,11 @@
+class Demo:
+    def addition(self,a,b):
+        print(a+b)
+     def subtraction(self,a,b):
+        print(a-b)
+     def multiply(self,a,b):
+        print(a*b)
+d1=Demo()
+d1.addition(6,14)
+d1.subtraction(11,14)
+d1.multiply(1,12)
